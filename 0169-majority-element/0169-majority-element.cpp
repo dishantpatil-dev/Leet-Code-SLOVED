0ab8a084c71804239b727744ed4ffd95d5cd4102
DISHANT PATIL <dishantvpatil@gmail.com>
2026-09-09 18:40:04 +0530

    Time: 0 ms (100%), Space: 28.1 MB (95.13%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq=0,n=nums.size(),ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(freq==0)
+            {ans=nums[i];}
+            if(ans==nums[i])
+            {freq++;}
+            else{freq--;}
+        }
+        return ans;
+        
+    }
+};
