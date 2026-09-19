9ae67c2c26353a3f863374a386b676f35162eb45
DISHANT PATIL <dishantvpatil@gmail.com>
2026-09-20 01:48:01 +0530

    Time: 4 ms (65.19%), Space: 13.5 MB (52.31%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,11 +3,10 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans(2*n);
         int i=0,j=n;
-        for( int k=0;k<2*n;k++)
+        for( int k=0;k<2*n;k+=2)
         {
             ans[k]=nums[i];
-            k++;
-            ans[k]=nums[j];
+            ans[k+1]=nums[j];
             i++;
             j++;
         }
