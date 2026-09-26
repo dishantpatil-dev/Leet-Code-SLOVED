18c6be3503b6c2fa6ccbbd447539f486cdc1602b
DISHANT PATIL <dishantvpatil@gmail.com>
2026-09-26 11:31:51 +0530

    Time: 64 ms (91.9%), Space: 109 MB (99.38%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     string evaluate(string s, vector<vector<string>>& knowledge) {
-        unordered_map<string, string> k_map;
+      unordered_map<string_view, const string*> k_map;
+        k_map.reserve(knowledge.size());
         for (const auto& pair : knowledge) {
-            k_map[pair[0]] = pair[1];
+            k_map[pair[0]] = &pair[1];
         }
-
-        string res = "";
-        string key = "";
-        bool in_bracket = false;
-
-        // Step 2: Parse string
-        for (char c : s) {
-            if (c == '(') {
-                in_bracket = true;
-            } else if (c == ')') {
-                in_bracket = false;
-                auto it = k_map.find(key);
-                if (it != k_map.end()) {
-                    res += it->second;
-                } else {
-                    res += '?';
-                }
-                key.clear();
+        string res;
+        res.reserve(s.length());
+        size_t start = 0;
+        size_t pos = 0;
+        while ((pos = s.find('(', start)) != string::npos) {
+            res.append(s, start, pos - start);
+            size_t close_pos = s.find(')', pos + 1);
+            string_view key(&s[pos + 1], close_pos - pos - 1);
+            auto it = k_map.find(key);
+            if (it != k_map.end()) {
+                res.append(*it->second);
             } else {
-                if (in_bracket) {
-                    key += c;
-                } else {
-                    res += c;
-                }
+                res.push_back('?');
             }
+            start = close_pos + 1;
+        }
+        if (start < s.length()) {
+            res.append(s, start, s.length() - start);
         }
 
         return res;
